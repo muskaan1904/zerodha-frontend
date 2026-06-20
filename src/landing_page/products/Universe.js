c5c171c2c680e08c5fa46a571b28db7a53a8b36f
muskan <159767445+muskaan1904@users.noreply.github.com>
2026-06-21 00:18:37 +0530

    Fix image alt attributes

--- src/landing_page/products/Universe.js
+++ src/landing_page/products/Universe.js
@@ -7,15 +7,15 @@ import React from 'react'
         <p>Extend your trading and investment experience even further with our partner platforms</p>
        
         <div className="col-4 p-3 mt-5">
-          <img src= "media/images/smallcaseLogo.png"  alt="Smallcase Logo/>
+          <img src= "media/images/smallcaseLogo.png"  alt="Smallcase Logo"/>
           <p className='text-small text-muted'>Thematic investment platform</p>
         </div> 
         <div className="col-4 p-3 mt-5">
-          <img src= "media/images/smallcaseLogo.png"  alt="Smallcase Logo/>
+          <img src= "media/images/smallcaseLogo.png"  alt="Smallcase Logo"/>
           <p className='text-small text-muted'>Thematic investment platform</p>
         </div>
         <div className="col-4 p-3 mt-5">
-          <img src= "media/images/smallcaseLogo.png"  alt="Smallcase Logo/>
+          <img src= "media/images/smallcaseLogo.png"  alt="Smallcase Logo"/>
           <p className='text-small text-muted'>Thematic investment platform</p>
         </div>
 
@@ -31,7 +31,7 @@ import React from 'react'
           <p className='text-small text-muted'>Bonds trading platform</p>
         </div>
         <div className="col-4 p-3 mt-5">
-          <img src= "media/images/dittoLogo.png" style={{height:"60px"}} alt="Smallcase Logo"   alt="Ditto Logo"/>
+          <img src= "media/images/dittoLogo.png" style={{height:"60px"}} alt="Ditto Logo"/>
           <p className='text-small text-muted'>Insurance</p>
         </div>
  <button className='p-2 btn btn-primary fs-5 mb-5' style={{width:"20%", margin: "0 auto"}}>Signup Now</button>
