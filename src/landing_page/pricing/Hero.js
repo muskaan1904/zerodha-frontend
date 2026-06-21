41c764a6cfad26fcb03693b56fd3b53ac9ab00b8
muskan <159767445+muskaan1904@users.noreply.github.com>
2026-06-21 14:48:43 +0530

    final cleanup before deploy

--- src/landing_page/pricing/Hero.js
+++ src/landing_page/pricing/Hero.js
@@ -9,22 +9,22 @@ import React from 'react'
 
 <div className='row p-5 mt-5 text-center'>
 <div className='col-4 p-4'>
-     <img src='media/images/pricingEquity.svg'  alt="Free equity delivery"/>
+     <img src='media/images/pricingEquity.svg'  alt="Free delivery"/>
      <h1 className='fs-3'> Free equity delivery</h1>
      <p className='text-muted'>All equity delivery investments (NSE,BSE), are absolutely free - ₹0 brokerage.</p>
    </div>
 
 
 <div className='col-4 p-4'>
-     <img src='media/images/intradayTrades.svg' alt="Intraday trading"/>
+     <img src='media/images/intradayTrades.svg'  alt="Intraday trading"/>
      <h1 className='fs-3'>Intraday and F&0 trades</h1>
-     <p className='texxt-muted'>Flat Rs. 20 or 0.03% (whichever is lower) per excuted order on intraday trades across equity, currency, and commodity trades.</p>
+     <p className='text-muted'>Flat Rs. 20 or 0.03% (whichever is lower) per excuted order on intraday trades across equity, currency, and commodity trades.</p>
    </div>
 
 <div className='col-4 p-4'>
-     <img src='media/images/intradayTrades.svg'  alt="Equity pricing"/>
+     <img src='media/images/intradayTrades.svg' alt="Equity pricing"/>
      <h1 className='fs-3'>Free direct MF</h1>
-     <p className='texxt-muted'>All equity delivery investments (NSE, BSE), are absolutely free - </p>
+     <p className='text-muted'>All equity delivery investments (NSE, BSE), are absolutely free - </p>
    </div>
 </div>
       </div>
