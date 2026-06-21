c0c6b719fafdd1527532bc5b31cd66b85a1fa307
muskan <159767445+muskaan1904@users.noreply.github.com>
2026-06-21 17:35:26 +0530

    force env fix

--- src/landing_page/signup/Signup.jsx
+++ src/landing_page/signup/Signup.jsx
@@ -6,6 +6,7 @@ import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
 
 const API_URL = import.meta.env.VITE_API_URL;
+console.log("API_URL:", API_URL);
 function Signup() {
   const navigate = useNavigate();
   useEffect(() => {
